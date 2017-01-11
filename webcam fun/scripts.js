e7e3b8cae34d8917a66d6e8306c10d1568ad43aa
Ryan <rheupler@gmail.com>
2017-01-10 21:28:39 -0500

    adds functioning camera and ability to take and download photos

--- webcam fun/scripts.js
+++ webcam fun/scripts.js
@@ -3,3 +3,51 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+function getVideo() {
+  navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+    .then(localMediaStream => {
+      video.src = window.URL.createObjectURL(localMediaStream);
+      console.log(localMediaStream);
+      video.play();
+    })
+    .catch(err => {
+      console.error("You've done it now...", err);
+    })
+}
+
+function paintToCanvas() {
+  const width = video.videoWidth;
+  const height = video.videoHeight;
+  canvas.width = width;
+  canvas.height = height;
+
+  return setInterval(() => {
+    ctx.drawImage(video, 0, 0, width, height);
+    let pixels = ctx.getImageData(0, 0, width, height);
+    pixels = redEffect(pixels);
+  }, 16);
+}
+
+function takePhoto() {
+  snap.currenTime = 0;
+  snap.play()
+
+  const data = canvas.toDataURL('image/jpeg');
+  const link = document.createElement('a');
+  link.href = data;
+  link.setAttribute('download', 'handsome');
+  link.innerHTML = `<img src="${data} alt"Handsome Man" />`
+  strip.insertBefore(link, strip.firstChild);
+}
+
+function redEffect() {
+  for(let i = 0; < pixels.length; i += 4) {
+    pixels[i];
+    pixels[i + 1];
+    pixels[i + 2];
+  }
+}
+
+video.addEventListener('canplay', paintToCanvas);
+// getVideo();
