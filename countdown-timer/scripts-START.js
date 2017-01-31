5c4b6ac225f06e1734eb62cccd13f68feea6e659
Ryan <rheupler@gmail.com>
2017-01-30 20:41:59 -0500

    3 quarters

--- countdown-timer/scripts-START.js
+++ countdown-timer/scripts-START.js
@@ -1,19 +1,34 @@
 let countdown;
+const timerDisplay = document.querySelector('.display__time-left')
+const endTime = document.querySelector('.display__end-time');
+const buttons = document.querySelectorAll('[data-time]');
 
 function timer(seconds) {
   const now = Date.now();
   const then = now + seconds * 1000;
-  displaySecondsLeft(seconds)
+  displayTimeLeft(seconds)
+  displayEndTime(then)
 
   countdown = setInterval(() => {
     const secondsLeft = Math.round((then - Date.now()) / 1000);
     if(secondsLeft < 0) {
       clearInterval(countdown)
     }
-    displaySecondsLeft(secondsLeft)
+    displayTimeLeft(secondsLeft)
   }, 1000)
 }
 
-function displaySecondsLeft(seconds) {
-  console.log(seconds)
+function displayTimeLeft(seconds) {
+  const minutes = Math.floor(seconds / 60);
+  const remainderSeconds = seconds % 60;
+  const display = `${minutes}:${remainderSeconds < 10 ? `0` : ''}${remainderSeconds}`
+  timerDisplay.textContent = display;
+  document.title = display;
+}
+
+function displayEndTime(timestamp) {
+  const end = new Date(timestamp);
+  const hours = end.getHours();
+  const minutes = end.getMinutes();
+  endTime.textContent = `Be Back At ${hours > 12 ? hours - 12 : hours}:${minutes < 10 ? '0' : ''}${minutes}`;
 }
